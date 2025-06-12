5542c7494a8faf021bb47ea77ed6c5726b319ac2
KiwiKey <kiwikey.vn@gmail.com>
2025-06-12 22:44:46 +0700

    Test02

--- src/custom_code.c
+++ src/custom_code.c
@@ -10,6 +10,7 @@
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 static int raw_hid_received_event_listener(const zmk_event_t *eh) {
     struct raw_hid_received_event *event = as_raw_hid_received_event(eh);
+	prinft("hihihihihihihihihihi");
     if (event->data[0]==0) {
 		LOG_INF("display_process_raw_hid_data - received data_type %u", event->data[0]);
 		// zmk_keymap_layer_to(0);
